c6d517f3cac5dfd04fc00722eff99fa5d5187862
Pedro Ollero <pedroollero1998@gmail.com>
2025-03-13 19:26:26 +0100

    refactor order number handling to include input submission and validation in main.ts; update script loading in index.html

--- src/main.ts
+++ src/main.ts
@@ -1,43 +1,54 @@
-const resetButton = document.getElementById('botones__reset') as HTMLButtonElement;
-const backButton = document.getElementById('botones__anterior') as HTMLButtonElement;
-const nextButton = document.getElementById('botones__siguiente') as HTMLButtonElement;
-const textInput = document.getElementById('form__input') as HTMLInputElement;
-const submitButton = document.getElementById('form__button') as HTMLButtonElement;
-const boardNumber = document.getElementById('numero-turno') as any;
+const resetButton = document.getElementById(
+  "botones__reset"
+) as HTMLButtonElement;
+const backButton = document.getElementById(
+  "botones__anterior"
+) as HTMLButtonElement;
+const nextButton = document.getElementById(
+  "botones__siguiente"
+) as HTMLButtonElement;
+const textInput = document.getElementById("form__input") as HTMLInputElement;
+const submitButton = document.getElementById(
+  "form__button"
+) as HTMLButtonElement;
+const boardNumber = document.getElementById("numero-turno") as any;
 
+let orderNumber: number = 0;
 
+function boardNumberHandler() {
 
+  const impressHandler = (newOrder: number) => {
+    const orderString: string = newOrder.toString().padStart(2, "0");
+    boardNumber.innerHTML = `${orderString}`;
+  };
 
-function boardNumberHandler(){
-    let orderNumber: number = 0;
+  submitButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    let newOrder = parseInt(textInput.value);
+    orderNumber = newOrder
+    impressHandler(orderNumber);
+  });
 
-    const impressHandler = (newOrder: number) =>{
-        const orderString: string = newOrder.toString();
-         boardNumber.innerHTML = `${orderString}`;
-    }   
+  nextButton.addEventListener("click", () => {
+    orderNumber++;
+    impressHandler(orderNumber);
+  });
+
+  backButton.addEventListener("click", () => {
+    if(orderNumber > 0){
+        orderNumber--;
+    impressHandler(orderNumber);
+    } else{
+        alert("No puede haber un turno menor de 0")
+    }
     
-    nextButton.addEventListener('click', () =>{
-        orderNumber++;
-        impressHandler(orderNumber);
-    })
+  });
 
-    backButton.addEventListener('click', () =>{
-        orderNumber--;
-        impressHandler(orderNumber);
-    })
-
-    backButton.addEventListener('click', () =>{
-        orderNumber--;
-        impressHandler(orderNumber);
-    })
-
-
+  resetButton.addEventListener("click", () => {
+    orderNumber = 0;
+    impressHandler(orderNumber);
+  });
 
 }
 
 boardNumberHandler();
-
-
-
-
-
