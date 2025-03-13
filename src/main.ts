1a537257bbd546c7ada8585d61e5391f658f1d26
Pedro Ollero <pedroollero1998@gmail.com>
2025-03-13 18:07:52 +0100

    refactor button and input elements with IDs for better accessibility and functionality in index.html and main.ts; update styles in style.css

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,20 @@
-import "./style.css";
+const resetButton = document.getElementById('botones__reset') as HTMLButtonElement;
+const backButton = document.getElementById('botones__anterior') as HTMLButtonElement;
+const nextButton = document.getElementById('botones__siguiente') as HTMLButtonElement;
+const textInput = document.getElementById('form__input') as HTMLInputElement;
+const submitButton = document.getElementById('form__button') as HTMLButtonElement;
+const boardNumber = document.getElementById('numero-turno') as HTMLElement;
 
-console.log("Hello Typescript!");
+
+const orderNumber: number = 0;
+
+function boardNumberHandler(order: number){
+    const orderString: string = order.toString();
+    boardNumber.innerHTML = `${orderString}`;
+}
+
+function buttonsHandler(){
+    
+}
+
+boardNumberHandler(orderNumber);
