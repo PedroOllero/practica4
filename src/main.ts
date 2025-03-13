3a15203bd8d159ce245b738b41ac61cb9860f42b
Pedro Ollero <pedroollero1998@gmail.com>
2025-03-13 18:27:31 +0100

    refactor board number handling to use local orderNumber variable and add event listeners for button clicks in main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,35 @@ const backButton = document.getElementById('botones__anterior') as HTMLButtonEle
 const nextButton = document.getElementById('botones__siguiente') as HTMLButtonElement;
 const textInput = document.getElementById('form__input') as HTMLInputElement;
 const submitButton = document.getElementById('form__button') as HTMLButtonElement;
-const boardNumber = document.getElementById('numero-turno') as HTMLElement;
+const boardNumber = document.getElementById('numero-turno') as any;
 
 
-const orderNumber: number = 0;
 
-function boardNumberHandler(order: number){
-    const orderString: string = order.toString();
+
+function boardNumberHandler(){
+    let orderNumber: number = 0;
+    const orderString: string = orderNumber.toString();
     boardNumber.innerHTML = `${orderString}`;
+
+    nextButton.addEventListener('click', () =>{
+        orderNumber++;
+        const orderString: string = orderNumber.toString();
+        boardNumber.innerHTML = `${orderString}`;
+    })
+
+    backButton.addEventListener('click', () =>{
+        orderNumber--;
+        const orderString: string = orderNumber.toString();
+        boardNumber.innerHTML = `${orderString}`;
+    })
+
+
+
 }
 
-function buttonsHandler(){
-    
-}
+boardNumberHandler();
 
-boardNumberHandler(orderNumber);
+
+
+
+
